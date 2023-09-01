593c77cc772f731847862a5521aa2b18fa5e3e44
gyy <g18355735449@163.com>
2023-09-01 13:54:02 +0800

    initial

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import MyTree from './components/MyTree.vue'
+</script>
+
+<template>
+  <div class="sidebar">
+    <MyTree />
+  </div>
+</template>
+
+<style scoped>
+.sidebar {
+  width: 300px;
+  background-color: #f3f4f6;
+  border-right: 1px solid #dcdfe6;
+  padding: 20px;
+  border: 2px solid blue;
+}
+</style>
